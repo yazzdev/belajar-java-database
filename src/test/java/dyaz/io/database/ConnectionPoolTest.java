f95072739065f4c6030b42be1ee1eec068f5f00e
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2025-05-08 19:51:18 +0700

    DataSource

---
+++ src/test/java/dyaz/io/database/ConnectionPoolTest.java
@@ -0,0 +1,41 @@
+package dyaz.io.database;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionPoolTest {
+
+  @Test
+  void testHikariCP() {
+    HikariConfig config = new HikariConfig();
+    config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+    config.setJdbcUrl("jdbc:mysql://localhost:3306/belajar_java_database");
+    config.setUsername("root");
+    config.setPassword("");
+
+    // Config Pool
+    config.setMaximumPoolSize(10);
+    config.setMinimumIdle(5);
+    config.setIdleTimeout(60_000);
+    config.setMaxLifetime(10 * 60_000);
+
+    try {
+      HikariDataSource dataSource = new HikariDataSource(config);
+      Connection connection = dataSource.getConnection();
+      connection.close();
+      dataSource.close();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  // Connection From main -> Connection Util
+  @Test
+  void testConnectionUtil() throws SQLException {
+    Connection connection = ConnectionUtil.getDataSource().getConnection();
+  }
+}
