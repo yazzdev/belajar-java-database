31d171ce1164b5a54d6aee1f6bef5916f05193c7
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2025-05-11 12:55:12 +0700

    Statement

---
+++ src/test/java/dyaz/io/database/StatementTest.java
@@ -0,0 +1,76 @@
+package dyaz.io.database;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class StatementTest {
+
+  @Test
+  void tesetCreateStatement() throws SQLException {
+
+    Connection connection = ConnectionUtil.getDataSource().getConnection();
+    Statement statement = connection.createStatement();
+
+    statement.close();
+    connection.close();
+
+  }
+
+  @Test
+  void tesetExecuteUpdate() throws SQLException {
+
+    Connection connection = ConnectionUtil.getDataSource().getConnection();
+    Statement statement = connection.createStatement();
+
+    String sql = """
+            INSERT INTO customers(id, name, email)
+            VALUES ('124', 'Amrullah', 'amrullah@email.com')
+            """;
+    int update = statement.executeUpdate(sql);
+    System.out.println(update);
+
+
+    statement.close();
+    connection.close();
+
+  }
+
+  @Test
+  void tesetExecuteDelete() throws SQLException {
+
+    Connection connection = ConnectionUtil.getDataSource().getConnection();
+    Statement statement = connection.createStatement();
+
+    String sql = """
+            DELETE FROM customers;
+            """;
+    int update = statement.executeUpdate(sql);
+    System.out.println(update);
+
+
+    statement.close();
+    connection.close();
+
+  }
+
+  @Test
+  void tesetExecuteQuery() throws SQLException {
+
+    Connection connection = ConnectionUtil.getDataSource().getConnection();
+    Statement statement = connection.createStatement();
+
+    String sql = """
+            SELECT * FROM customers;
+            """;
+    ResultSet resultSet = statement.executeQuery(sql);
+
+    resultSet.close();
+    statement.close();
+    connection.close();
+
+  }
+}
